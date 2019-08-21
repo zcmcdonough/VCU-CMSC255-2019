983c5537fe352cfd71b1e8eff5cec375189d682e
zcmcdonough <54368793+zcmcdonough@users.noreply.github.com>
2019-08-21 12:03:01 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,25 @@
+/* Java Program Name
+*****************************************************************8
+*Project/Program Description
+*
+*******************************************************************
+*Zachariah McDonough
+*8/21/19
+*CMSC 255 Section 4
+**********************************************************************/
+
+public class Hello {
+	public static void printHeading() {
+		System.out.println("Your Name");
+		System.out.println("Java Project Name");
+		System.out.println("Project Description");
+		System.out.println("Version Date");
+		System.out.println("Course Number and Section");
+		System.out.println();
+	}
+	
+	public static void main(String [] args) {
+		printHeading();
+		System.out.println("Hello");
+	}
+}
